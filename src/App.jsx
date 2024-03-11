603f4f1a17d7e7aff3642dd83f2b4a9dd1f23b09
Fac3Hunter <shevchenko.tech@gmail.com>
2024-03-11 14:47:41 +0300

    "project Initial"

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import ButtonGradient from "./assets/svg/ButtonGradient";
+
+const App = () => {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">HELLO WORLD!</h1>
+
+      <ButtonGradient />
+    </>
+  );
+};
+export default App;
