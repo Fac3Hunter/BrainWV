ad3c83ed539d1b78c557b55148796a9df421fac1
Fac3Hunter <shevchenko.tech@gmail.com>
2024-03-13 13:59:07 +0300

    menu header & adaptive layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 const App = () => {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">HELLO WORLD!</h1>
-
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
+        <Hero />
+      </div>
       <ButtonGradient />
     </>
   );
